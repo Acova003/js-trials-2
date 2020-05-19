6eb7ae263d869dd43180c553002f5125de66ef34
Amee <ameecova@yahoo.com>
2020-05-19 16:54:13 -0700

    finished functions

--- functions.js
+++ functions.js
@@ -5,12 +5,24 @@
 
 // Define your function here
 
+function isHometown(town){
+  console.log(town == "Hayward")};
+}
+
 
 // 2. getFullName
 
 // Define your function here
+function fullName(fname, lname){
+  console.log(fname + lname)
+}
 
 
 // 3. calculateTotal
 
 // Define your function here
+function fullName(fname, lname){
+    const message = `${fname} ${lname}`;
+
+    console.log(message)
+}
